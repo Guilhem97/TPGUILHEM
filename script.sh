122fd37fc6c4a922621f3bcf500d08f35f20de29
Guilhem97 <118891103+Guilhem97@users.noreply.github.com>
2022-12-06 21:22:39 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,12 +3,20 @@
 
 # Copier le fichier de configuration sur le serveur
 
-cp ./config__files/ssh/sshd_config /etc/ssh/sshd_config
+cp ./config__files/sshd_config /etc/ssh/sshd_config
 
 cp ./config__files/ssh/Banner /etc/Banner
 
+echo "Entrer un nom d'utilisateur : \n"
+read username 
+adduser $username
+usermod -aG sudo $username
 
-# Redemarrer le service sshd 
+echo "Entrer un mot de passe : \n"
+passwd $username
 
+echo "entrer une clé public : \n"
+read KeySsh
+echo $KeySsh >> ~/.ssh/authorized_keys
 systemctl restart sshd 
 
